da8b3b62cd7e24ce1f529421f68544df3393249e
INRIM Quantum Electrical Metrology <33510937+INRIMQuantumElectricalMetrology@users.noreply.github.com>
2026-02-07 07:48:53 +0100

    Add files via upload

---
+++ cable_cabiati_C1_1000pF.m
@@ -0,0 +1,21 @@
+%Capacitance standard
+CHL = 1000E-12; %Capacitance to be measured (2-port, high to low)
+DHL = 1e-5; %and loss
+%CH is capacitor (with the defining cable)
+CH = 149.82E-12 + (1166E-12-CHL); %Cable H and capacitance from port to ground
+
+%Cable 1: connection from transformer port 0.1 to the post where 1000 pF is connected
+%(with its own cable). There is a coax equalizer.
+C1 = 486.09E-12; 
+D1 = 0.00082;
+L1 = 1.61E-6;
+R1 = 144E-3;
+%
+%Cable 2: low side NOT CONSIDERED
+C2 = 0;
+D2 = 0;
+L2 = 0;
+R2 = 0;
+%
+%Operation
+f = 1592;
